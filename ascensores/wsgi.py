6c8a21d653402568a7851edd834abc38d05fff55
NicolasSalvay <nicolassalvay@yahoo.com.ar>
2025-07-20 21:18:54 -0300

    Modificacion settings y wsgi para prod

--- ascensores/wsgi.py
+++ ascensores/wsgi.py
@@ -1,16 +1,9 @@
-"""
-WSGI config for ascensores project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
-"""
-
 import os
-
+import sys
+path = '/var/www/ascensores'
+if path not in sys.path:
+    sys.path.append(path)
+os.environ["DJANGO_SETTINGS_MODULE"]="ascensores.settings"
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ascensores.settings")
-
 application = get_wsgi_application()
+
