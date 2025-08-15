8c63b79bb57710ead8cc02f1ddc041f642c8f746
NicolasSalvay <nicolassalvay@yahoo.com.ar>
2025-08-15 20:50:59 -0300

    Carrito parte 2 botones +-

--- carro/urls.py
+++ carro/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from carro.views_carrito import Carrito
+from carro import views_agregar
+from carro import views_quitar
 
 urlpatterns = [
     path("", Carrito.as_view(), name="carrito"),
+    path("agregar/", views_agregar.agregar, name="agregar"),
+    path("quitar/", views_quitar.quitar, name="quitar"),
 ]
--- static_dev/js/carro.js
+++ static_dev/js/carro.js
@@ -1,21 +1,32 @@
 
+/*..............................................................................................
+... PARA VALIDAR LOS DATOS .....................................................
+.............................................................................................*/
+var csrftoken = $.cookie('csrftoken');
+function csrfSafeMethod(method) {
+    "use strict";
+    return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+}   
+
+$.ajaxSetup({
+    beforeSend: function(xhr, settings) {
+        if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
+            xhr.setRequestHeader("X-CSRFToken", csrftoken);
+        }
+    }
+});
 
 /*-------------------------------------------------------------------------
---------- PASO 3 INCREMENTA CANTIDAD DE PRODUCTO EN LA TIENDA -------------
+---------  INCREMENTAR CANTIDAD DE PRODUCTO EN LA TIENDA -------------
 -------------------------------------------------------------------------*/
 function Agregar(cada_producto_id, valor) {
     "use strict";
-    $.ajax({
-        beforeSend : function (xhr, settings) {
-            if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
-                xhr.setRequestHeader("X-CSRFToken", csrftoken);
-            }
-        },
-        url : "/tienda/agregar/",
+    $.ajax({        
+        url : "/carrito/agregar/",
         type : "GET",
         data : { cada_producto_id:cada_producto_id, valor:valor},
         success : function (json) {
-            localStorage.setItem(json[0].idproducto.toString(), json[0].cantida.toString());
+            localStorage.setItem(json[0].idproducto.toString(), json[0].cantidad.toString());
             location.reload();
         },
         error : function (xhr, errmsg, err) {
@@ -23,18 +34,20 @@ function Agregar(cada_producto_id, valor) {
         }
     });
 }
-$('.agregar').click(function (event) {
+
+$('.btn-increment').click(function (event) {
     "use strict";
     event.preventDefault(); 
-    let cada_producto_id = $(this).parent().get(0).id;
-    let valor = $(this).parent().find('.vervalor').val();
+    let cada_producto_id = $(this).closest('form').find('input[name="producto"]').val();
+    let valor = $(this).parent().find('.cantidad-input').val();
     let i;
     for(i = 0; i < localStorage.length; i++){
         let clave_eliminar = localStorage.key(i);
-        if(!clave_eliminar.startsWith("prestige_")){
+        if(!clave_eliminar.startsWith("prod_")){
             localStorage.removeItem(clave_eliminar);
         }
     }
+
     for(i = 0; i < localStorage.length; i++){
         let clave = localStorage.key(i);
         let el_valor = localStorage[clave];
@@ -45,4 +58,68 @@ $('.agregar').click(function (event) {
         }   
     }
    Agregar(cada_producto_id, valor);
+});
+
+/*-------------------------------------------------------------------------
+---------  QUITAR CANTIDAD DE PRODUCTO EN LA TIENDA -------------
+-------------------------------------------------------------------------*/
+function Quitar(cada_producto_id, valor) {
+    "use strict";
+    $.ajax({        
+        url : "/carrito/quitar/",
+        type : "GET",
+        data : { cada_producto_id:cada_producto_id, valor:valor},
+		success : function (json) {
+            $('#'+json[0].idproducto +' .cantidad-input').val(json[0].cantidad);
+            let cant = json[0].cantidad
+            if (cant == 0){
+                console.log("vacío");
+                localStorage.removeItem(json[0].idproducto.toString(), json[0].cantidad.toString());
+                $.ajax({
+                    url: "/tienda/crear_localstorage/",
+                    data:{producto : JSON.stringify(localStorage)},
+                    type: 'get',
+                    dataType: 'json',
+                    contentType: 'application/json',
+                    success: function (data) {
+                        var urla = window.location.origin + "/carrito";    
+                        window.location.href = urla;
+                    },
+                });
+                location.reload();
+            }else{
+                localStorage.setItem(json[0].idproducto.toString(), json[0].cantidad.toString());
+                location.reload();
+            }
+            location.reload();
+		},
+        error : function (xhr, errmsg, err) {
+            console.log('Error en carga de respuesta');
+        }
+    });
+}
+
+$('.btn-decrement').click(function (event) {
+    "use strict";
+    event.preventDefault(); 
+    let cada_producto_id = $(this).closest('form').find('input[name="producto"]').val();
+    let valor = $(this).parent().find('.cantidad-input').val();
+    let i;
+    for(i = 0; i < localStorage.length; i++){
+        let clave_eliminar = localStorage.key(i);
+        if(!clave_eliminar.startsWith("prod_")){
+            localStorage.removeItem(clave_eliminar);
+        }
+    }
+
+    for(i = 0; i < localStorage.length; i++){
+        let clave = localStorage.key(i);
+        let el_valor = localStorage[clave];
+        if(clave == cada_producto_id){
+            valor = el_valor;
+        }else{
+            console.log("no hay coincidenciaaaa");
+        }   
+    }
+   Quitar(cada_producto_id, valor);
 });
