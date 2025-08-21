4177cef0d662a8388e4b883250aaba02ef3f3218
NicolasSalvay <nicolassalvay@yahoo.com.ar>
2025-08-20 21:04:41 -0300

    sendgrid 4

--- ascensores/settings.py
+++ ascensores/settings.py
@@ -202,4 +202,4 @@ EMAIL_BACKEND = "sendgrid_backend.SendgridBackend"
 DEFAULT_FROM_EMAIL = 'nicolassalvay.claro@gmail.com'
 
 #EMAIL_HOST_PASSWORD = os.environ.get('SENDGRID_API_KEY')
-EMAIL_HOST_PASSWORD = "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"
+SENDGRID_API_KEY = "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"
